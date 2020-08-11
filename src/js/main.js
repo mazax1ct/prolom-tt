e2bbf0604ac8e19cfe14e2b3030915df83ccc6d5
mazax1ct <vova@giveandget.ru>
2020-08-11 17:16:47 +0300

    new landing

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+var resize_scroll = function(e) {
+  var bH = $('.main-banner').height() - $('.header__bottom').height();
+  var h = $(".header");
+
+  if($(window).scrollTop() > bH) {
+    h.addClass("scrolled");
+  } else {
+    h.removeClass("scrolled");
+  }
+};
+
+$(document).ready(function () {
+  resize_scroll();
+});
+
+$(window).on("scroll", resize_scroll).on("resize", resize_scroll);
+
+$(document).on('click', '.js-menu-open', function () {
+  $('body').addClass('menu-open');
+  $('.header__menu-block').addClass('is-open');
+  return false;
+});
+
+$(document).on('click', '.js-menu-close', function () {
+  $('body').removeClass('menu-open');
+  $('.header__menu-block').removeClass('is-open');
+  return false;
+});
+
+$(document).on('click', '.main-menu__link', function () {
+  $('body').removeClass('menu-open');
+  $('.header__menu-block').removeClass('is-open');
+
+  $('.main-menu__link').removeClass('is-active');
+  $(this).addClass('is-active');
+
+  var offset = $($(this).attr('href')).offset().top;
+  var header = $('.header').height();
+  var offsetTop = offset - header;
+
+  $('html, body').animate({scrollTop:offsetTop}, '1000');
+  return false;
+});
+
+$(document).on('click', '.js-question', function () {
+  $(this).toggleClass('is-active');
+  $(this).next('.answer').slideToggle();
+  return false;
+});
